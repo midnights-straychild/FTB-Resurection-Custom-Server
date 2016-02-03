ea71bd65c9abfbe9be22ce7f31e455529485ea4d
Ralf Lauseker <midnights.straychild@gmail.com>
2016-02-03 20:55:43 +0100

    mingled with some confs

--- ServerStart.sh
+++ ServerStart.sh
@@ -4,4 +4,4 @@ if [ ! -f minecraft_server.1.7.10.jar ]; then
    ./FTBInstall.sh
 fi
  echo "make sure to read eula.txt before playing!"
- java -server -Xmn1G -Xms8192m -Xmx8192M -XX:PermSize=256m -XX:+UseG1GC -XX:+DisableExplicitGC -XX:+UseCompressedOops -XX:+UseCodeCacheFlushing -XX:MaxGCPauseMillis=200 -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:ParallelGCThreads=4 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -jar forge-1.7.10-10.13.4.1558-1.7.10-universal.jar nogui
+ java -server -Xmn1G -Xms8192m -Xmx8192M -XX:PermSize=256m -XX:+UseG1GC -XX:+DisableExplicitGC -XX:+UseCompressedOops -XX:+UseCodeCacheFlushing -XX:MaxGCPauseMillis=128 -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:ParallelGCThreads=4 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -jar forge-1.7.10-10.13.4.1558-1.7.10-universal.jar nogui
